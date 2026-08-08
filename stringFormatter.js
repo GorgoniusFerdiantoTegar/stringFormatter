1830dc66662bb524dc636daeb7f48eec8e792fc7
Naaaatsu <97505922+GorgoniusFerdiantoTegar@users.noreply.github.com>
2026-08-08 15:40:14 +0700

    string format done

---
+++ stringFormatter.js
@@ -0,0 +1,30 @@
+const cleanText = (string) => {
+    if (string == ''){
+        return ''
+    }
+    return string.trim()
+}
+
+const capitalize = (string) => {
+        if(undefined || string == ''){
+            return '';
+        }
+    return string[0].toUpperCase() + string.slice(1).toLowerCase();
+}
+
+const formatDisplayName = (firstName, lastName) => {
+    const firstname = capitalize(cleanText(firstName));
+    const lastname = capitalize(cleanText(lastName));
+
+    if (firstname == ''){
+        return `${lastname}`;
+    } else if (lastname == '') {
+        return `${firstname}`;
+    } else {
+        return `${firstname} ${lastname}`;
+    }
+}
+
+console.log(formatDisplayName('        ava', 'STONE  '));
+console.log(formatDisplayName('nOAh', '  kim'));
+console.log(formatDisplayName('    miNA   ', 'pATEL'));
